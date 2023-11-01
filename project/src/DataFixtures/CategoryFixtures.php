34dc40c68681dd71ec51bc8d4ee780a9489d6057
novapc74 <novapc@ukr.net>
2023-11-01 10:34:27 +0300

    add category fixtures

---
+++ project/src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends BaseFixture implements DependentFixtureInterface
+{
+
+    protected function loadData(ObjectManager $manager): void
+    {
+        $this->createEntity(Category::class, 1, function (Category $category) {
+            $category
+                ->setTitle('First category')
+                ->addProduct($this->getReference('Product_0'));
+
+        });
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProductFixtures::class
+        ];
+    }
+}
