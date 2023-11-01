9aba0d9236871247650ed49dd46488f3b80d15c0
novapc74 <novapc@ukr.net>
2023-11-01 18:28:59 +0300

    fix home_page_contoller

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -2,47 +2,40 @@
 
 namespace App\Controller;
 
-use App\Entity\PageBlock;
 use App\Entity\Product;
-use App\Enum\EnumTest;
 use App\Enum\PageBlockType;
-use App\Entity\PropertyGroup;
-use App\Entity\ProductPropertyValue;
-use App\Repository\PageBlockRepository;
 use App\Repository\ProductRepository;
-use App\Repository\PropertyGroupRepository;
+use App\Repository\PageBlockRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-	#[Route('/', name: 'app_home_page')]
-	public function index(PropertyGroupRepository $groupRepository,
-                          ProductRepository $productRepository,
-                          PageBlockRepository $pageBlockRepository): Response
-	{
-		return $this->render('pages/home.html.twig', [
-			'isHome' => true,
-			'products' => $productRepository->findBy([],[], 10),
+    #[Route('/', name: 'app_home_page')]
+    public function index(ProductRepository $productRepository, PageBlockRepository $pageBlockRepository): Response
+    {
+        return $this->render('pages/home.html.twig', [
+            'isHome' => true,
+            'products' => $productRepository->findBy([], [], 10),
             'main_blocks' => $pageBlockRepository->findBy(['type' => PageBlockType::product_block_type->name]),
             'interior_blocks' => $pageBlockRepository->findBy(['type' => PageBlockType::interior_block_type->name]),
-		]);
-	}
+        ]);
+    }
 
-	#[Route('/modification/{id}', name: 'app_product_modification')]
-	public function showProductModification(Request $request, ?Product $modification): JsonResponse
-	{
-		if ($request->isXmlHttpRequest() && $modification) {
+    #[Route('/modification/{id}', name: 'app_product_modification')]
+    public function showProductModification(Request $request, ?Product $modification): JsonResponse
+    {
+        if ($request->isXmlHttpRequest() && $modification) {
 
-			return $this->json($modification, 200, [], [
-				AbstractNormalizer::GROUPS => ['modification:item']
-			]);
-		}
+            return $this->json($modification, 200, [], [
+                AbstractNormalizer::GROUPS => ['modification:item']
+            ]);
+        }
 
-		return $this->json(['success' => false, 'message' => sprintf('для модификации "%s" сделайте запрос с заголовком XmlHttpRequest', $modification)], 404);
-	}
+        return $this->json(['success' => false, 'message' => sprintf('для модификации "%s" сделайте запрос с заголовком XmlHttpRequest', $modification)], 404);
+    }
 }
